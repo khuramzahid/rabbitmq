8cdd6b1bdbf8992fa2316b761cb907f5cc31d912
Khuram Zahid <khuramzahid@gmail.com>
2023-08-28 16:43:00 +0500

    Routing

--- routing/receive_logs_direct.js
+++ routing/receive_logs_direct.js
@@ -2,6 +2,13 @@
 
 var amqp = require('amqplib/callback_api');
 
+var args = process.argv.slice(2);
+
+if (args.length == 0) {
+    console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+    process.exit(1);
+}
+
 amqp.connect('amqp://localhost', function(error0, connection) {
     if (error0) {
         throw error0;
@@ -10,9 +17,9 @@ amqp.connect('amqp://localhost', function(error0, connection) {
         if (error1) {
             throw error1;
         }
-        var exchange = 'logs';
+        var exchange = 'direct_logs';
 
-        channel.assertExchange(exchange, 'fanout', {
+        channel.assertExchange(exchange, 'direct', {
             durable: false
         });
 
@@ -22,13 +29,14 @@ amqp.connect('amqp://localhost', function(error0, connection) {
             if (error2) {
                 throw error2;
             }
-            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-            channel.bindQueue(q.queue, exchange, '');
+            console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+            args.forEach(function(severity) {
+                channel.bindQueue(q.queue, exchange, severity);
+            });
 
             channel.consume(q.queue, function(msg) {
-                if (msg.content) {
-                    console.log(" [x] %s", msg.content.toString());
-                }
+                console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
             }, {
                 noAck: true
             });
