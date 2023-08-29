6eed591d33e6d06183d87301b105007991026aef
Khuram Zahid <khuramzahid@gmail.com>
2023-08-29 17:26:10 +0500

    Topic exchange

--- topics/emit_log_topic.js
+++ topics/emit_log_topic.js
@@ -10,16 +10,16 @@ amqp.connect('amqp://localhost', function(error0, connection) {
         if (error1) {
             throw error1;
         }
-        var exchange = 'direct_logs';
+        var exchange = 'topic_logs';
         var args = process.argv.slice(2);
+        var key = (args.length > 0) ? args[0] : 'anonymous.info';
         var msg = args.slice(1).join(' ') || 'Hello World!';
-        var severity = (args.length > 0) ? args[0] : 'info';
 
-        channel.assertExchange(exchange, 'direct', {
+        channel.assertExchange(exchange, 'topic', {
             durable: false
         });
-        channel.publish(exchange, severity, Buffer.from(msg));
-        console.log(" [x] Sent %s: '%s'", severity, msg);
+        channel.publish(exchange, key, Buffer.from(msg));
+        console.log(" [x] Sent %s: '%s'", key, msg);
     });
 
     setTimeout(function() {
--- topics/receive_logs_topic.js
+++ topics/receive_logs_topic.js
@@ -5,7 +5,7 @@ var amqp = require('amqplib/callback_api');
 var args = process.argv.slice(2);
 
 if (args.length == 0) {
-    console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+    console.log("Usage: receive_logs_topic.js <facility>.<severity>");
     process.exit(1);
 }
 
@@ -17,9 +17,9 @@ amqp.connect('amqp://localhost', function(error0, connection) {
         if (error1) {
             throw error1;
         }
-        var exchange = 'direct_logs';
+        var exchange = 'topic_logs';
 
-        channel.assertExchange(exchange, 'direct', {
+        channel.assertExchange(exchange, 'topic', {
             durable: false
         });
 
@@ -31,12 +31,12 @@ amqp.connect('amqp://localhost', function(error0, connection) {
             }
             console.log(' [*] Waiting for logs. To exit press CTRL+C');
 
-            args.forEach(function(severity) {
-                channel.bindQueue(q.queue, exchange, severity);
+            args.forEach(function(key) {
+                channel.bindQueue(q.queue, exchange, key);
             });
 
             channel.consume(q.queue, function(msg) {
-                console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+                console.log(" [x] %s:'%s'", msg.fields.routingKey, msg.content.toString());
             }, {
                 noAck: true
             });
