f9b82491e75c9d8c5eb89f579e2c97c327dd6daa
Khuram Zahid <khuramzahid@gmail.com>
2023-09-11 12:15:18 +0500

    direct changes

--- emit_log_direct.js
+++ emit_log_direct.js
@@ -2,14 +2,32 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+const connect = async (amqp, host) => {
+    return new Promise ((resolve, reject) => {
+        amqp.connect(host, (error, connection) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(connection);
+        });
+    });
+};
+
+const createChannel = async (connection) => {
+    return new Promise ((resolve, reject) => {
+        connection.createChannel((error, channel) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(channel);
+        });
+    });
+};
+
+async function task() {
+    const connection = await connect(amqp, 'amqp://localhost');
+    const channel = await createChannel(connection);
+    
         var exchange = 'direct_logs';
         var args = process.argv.slice(2);
         var msg = args.slice(1).join(' ') || 'Hello World!';
@@ -20,10 +38,11 @@ amqp.connect('amqp://localhost', function(error0, connection) {
         });
         channel.publish(exchange, severity, Buffer.from(msg));
         console.log(" [x] Sent %s: '%s'", severity, msg);
-    });
 
-    setTimeout(function() {
+    setTimeout(() => {
         connection.close();
         process.exit(0);
     }, 500);
-});
+}
+
+task();
